499501aa10fe1d395ed78b8ed6ffef35a57b428e
Laurie Lee <102129349+lauriehlee@users.noreply.github.com>
2023-02-25 18:35:51 -0500

    Update readData.py

--- readData.py
+++ readData.py
@@ -2,18 +2,21 @@ import pandas as pd
 from Group import Group
 from Person import Person
 import calculatePts
-import subprocess
+from apscheduler.schedulers.background import BackgroundScheduler
+from time import sleep
 
 df = pd.read_excel('healthData.xlsx')
+currentRow = 0
 
 group_point_goal = 1000
 group = Group([], group_point_goal, False)
 for x in range(len(df)):
     personData = []
+    ++currentRow
     for z in range(len(df.loc[x])):
         personData.append(df.loc[x][z])
 
-    new_person = Person(personData[0], personData[1], personData[3],0, personData[2])
+    new_person = Person(personData[0], 0, personData[2],0, personData[1])
     calculatePts.calc_pts(new_person)
     group.add_friend(new_person)
 
@@ -28,22 +31,31 @@ print(group.friends[2].name)
 print(group.friends[2].points)
 #delete previous data points in sheet to start over?
 
-#every hour: TODO
-for x in range(len(df)):
-    personData = []
-    for z in range(len(df.loc[x])): #start where previously stopped?
-        personData.append(df.loc[x][z])
 
-    for k in group.friends:
-        if k == personData[0]:
-            k.add_steps(personData[1])
-            k.add_exercise_time(personData[3])
-            calculatePts.calc_pts(k)
+#start for loop from row you last ended at TODO
+def update():
+    for x in range(len(df)):
+        personData = []
+        ++currentRow
+        for z in range(len(df.loc[x])): #start where previously stopped?
+            personData.append(df.loc[x][z])
 
+        for k in group.friends:
+            if k == personData[0]:
+                k.add_exercise_time(personData[3])
+                calculatePts.calc_pts(k)
 
-    new_person = Person(personData[0], personData[1], personData[3],0, personData[2])
-    calculatePts.calc_pts(new_person)
-    calculatePts.calc_group_pts(group)
+        calculatePts.calc_group_pts(group)
+
+#link steps to kelly's function instead of my own
+
+sched = BackgroundScheduler()
+sched.add_job(update, 'interval', seconds = 3600)
+sched.start()
+
+while True:
+    sleep(1)
+    
 
 #called every hour
 #takes in a path to a csv file (with the step data) and returns the number of steps taken in the last hour
