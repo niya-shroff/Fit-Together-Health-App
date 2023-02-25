50dc7d5cd2778a13f42f2576dc2a4a77bfae3498
Laurie Lee <102129349+lauriehlee@users.noreply.github.com>
2023-02-25 15:42:07 -0500

    Update personClass.py

--- personClass.py
+++ personClass.py
@@ -3,12 +3,14 @@ class Person:
     steps = 0
     exercise_time = 0
     points = 0
+    bpm = 0
 
-    def __init__(self, name, steps, exercise_time, points):
+    def __init__(self, name, steps, exercise_time, points, bpm):
         self.name = name
         self.steps = steps
         self.exercise_time = exercise_time
         self.points = points
+        self.bpm = bpm
 
     def add_steps(self, num_steps):
         #updated every hour
@@ -17,3 +19,6 @@ class Person:
     def add_exercise_time(self, num_exercise_time):
         #updated every hour
         self.exercise_time += num_exercise_time
+    def add_bpm(self, num_bpm):
+        self.bpm += num_bpm
+
