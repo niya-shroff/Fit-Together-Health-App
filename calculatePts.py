ccd124bcb6ed2b76fe05a59565be47fc5bdda6a7
Kelly Deng <102448307+kellyrdeng@users.noreply.github.com>
2023-02-25 13:01:37 -0500

    "added function to calculate group points"

--- calculatePts.py
+++ calculatePts.py
@@ -2,9 +2,14 @@ import math
 #function is called every hour
 #function takes in steps, amount of time in an hour that BPM is over 130 and calculates points for one person in that hour
 
-def calc_pts(steps: int, exercise_time: int):
-    step_pts = math.floor(steps / 100)
-    exercise_pts = math.floor(exercise_time / 10) * 20
-    return step_pts + exercise_pts
+def calc_pts(person):
+    step_pts = math.floor(person.steps / 100)
+    exercise_pts = math.floor(person.exercise_time / 10) * 20
+    person.points += step_pts + exercise_pts
 
-print(calc_pts(150, 20))
+#takes in a group of people and calculates their total points
+def calc_group_pts(group):
+    group_pts = group.curr_group_points
+    friends_arr = group.friends
+    for friend in friends_arr:
+        group_pts += friend.pts
