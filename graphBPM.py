55a60fa41a70576b66e94a6e4d0d108da30e731d
mesenther <89994144+mesenther@users.noreply.github.com>
2023-02-26 10:05:42 -0500

    Add files via upload

---
+++ graphBPM.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import numpy as np
+import random
+import serial
+
+#initialize serial port
+ser = serial.Serial()
+ser.port = '/dev/ttyACM0' #Arduino serial port
+ser.baudrate = 9600
+ser.timeout = 10 #specify timeout when using readline()
+ser.open()
+if ser.is_open==True:
+	print("\nAll right, serial port now open. Configuration:\n")
+	print(ser, "\n") #print serial parameters
+
+# Create figure for plotting
+fig = plt.figure()
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    #Aquire and parse data from serial port
+    line=ser.readline()      #ascii
+    line_as_list = line.split(b',')
+    i = int(line_as_list[0])
+    BPM = line_as_list[1]
+    relProb_as_list = BPM.split(b'\n')
+    relProb_float = float(relProb_as_list[0])
+	
