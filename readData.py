788067fe478e77ca338a2916b51c56e734e4340b
Laurie Lee <102129349+lauriehlee@users.noreply.github.com>
2023-02-25 19:16:14 -0500

    Update readData.py

--- readData.py
+++ readData.py
@@ -1,53 +1,26 @@
 import pandas as pd
-from classGroup import Group
-from personClass import Person
+from Group import Group
+from Person import Person
 import calculatePts
 from apscheduler.schedulers.background import BackgroundScheduler
 from time import sleep
 
-df = pd.read_excel('healthData.xlsx')
-currentRow = 0
 
-group_point_goal = 1000
-group = Group([], group_point_goal, False)
-for x in range(len(df)):
-    personData = []
-    ++currentRow
-    for z in range(len(df.loc[x])):
-        personData.append(df.loc[x][z])
+#this group's points must be updated
+    person1 = Person("Niya", 0, 0, 0, 0, 100)
+    person2 = Person("Kelly", 0, 0, 0, 0, 100)
+    groupArr = Group([person1, person2], 1000, False)
 
-    new_person = Person(personData[0], 0, personData[2],0, personData[1])
-    calculatePts.calc_pts(new_person)
-    group.add_friend(new_person)
-
-calculatePts.calc_group_pts(group)
-
-
-print(group.friends[0].name)
-print(group.friends[0].points)
-print(group.friends[1].name)
-print(group.friends[1].points)
-print(group.friends[2].name)
-print(group.friends[2].points)
-#delete previous data points in sheet to start over?
-
-
-#start for loop from row you last ended at TODO
 def update():
-    for x in range(len(df)):
-        personData = []
-        ++currentRow
-        for z in range(len(df.loc[x])): #start where previously stopped?
-            personData.append(df.loc[x][z])
+    for x in group:
+        steps = parse_step_CSV(PTAHHHAHSJDASHD)
+        x.addSteps(steps)
+        timeExercised = parse_exercise_time(SFDJDHSHsDFH)
+        x.add_exercise_time(timeExercised)
+        calculatePts.calc_pts(x)
 
-        for k in group.friends:
-            if k == personData[0]:
-                k.add_exercise_time(personData[3])
-                calculatePts.calc_pts(k)
+    calculatePts.calc_group_pts(group)
 
-        calculatePts.calc_group_pts(group)
-
-#link steps to kelly's function instead of my own
 
 sched = BackgroundScheduler()
 sched.add_job(update, 'interval', seconds = 3600)
